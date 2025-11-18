3606d35f1b8d624605176a128c0168dbe2ed6432
Xuezhi He <hexu1011@h-ka.de>
2025-11-18 01:28:05 +0100

    Add employee status update functionality

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -94,11 +94,25 @@ public class EmployeeController {
      * @param employeePageQueryDTO
      * @return
      */
-    @GetMapping
+    @GetMapping("/page")
     @ApiOperation("分页查询员工")
     public Result<PageResult> page (EmployeePageQueryDTO employeePageQueryDTO){
         log.info("员工分页查询，参数为 {}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 启用禁用员工
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工")
+    public Result startOrStop (@PathVariable Integer status, Long id){
+        log.info("启用禁用员工账号 {},{}", status,id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
     }
 }
