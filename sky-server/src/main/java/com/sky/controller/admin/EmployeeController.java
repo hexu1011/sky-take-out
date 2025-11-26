95001adc0b0c8528f357c331ad420029383c03b7
Xuezhi He <hexu1011@h-ka.de>
2025-11-26 05:44:15 +0100

    implement employee update feature

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -14,6 +14,7 @@ import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -113,6 +114,31 @@ public class EmployeeController {
     public Result startOrStop (@PathVariable Integer status, Long id){
         log.info("启用禁用员工账号 {},{}", status,id);
         employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查找员工
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查找员工")
+    public Result<Employee> getById (@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /**
+     * 编辑员工数据
+     * @param employeeDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("编辑员工数据")
+    public Result update (@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工数据: {}", employeeDTO);
+        employeeService.update(employeeDTO);
         return Result.success();
     }
 }
